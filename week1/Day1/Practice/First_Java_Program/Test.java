540d0d9e283cb0da9c1a6aff78e27bdb14f9ad6c
BENAMORNAWFAL <technobenamor@gmail.com>
2023-08-01 13:37:56 +0100

    First_Java_Test

---
+++ week1/Day1/Practice/First_Java_Program/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("Nawfal");
+        System.out.println("Benamor");
+        System.out.println("Tunisia");
+	}
+}
